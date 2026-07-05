9542c0e87d2c92583c4a25d313c173d030ad2bff
Alexandre Cheyroux <alexandre.cheyroux@outlook.com>
2026-07-05 10:23:44 +0200

    add oSNR comparison experiment

---
+++ code/scripts/Graphe oSNR/experience_osnr.py
@@ -0,0 +1,370 @@
+#Import des librairies
+import numpy as np
+import matplotlib.pyplot as plt
+import soundfile as sf
+import sys
+import csv
+from datetime import datetime
+import os
+import torch
+from denoiser.dsp import convert_audio
+
+sys.path.append("../../utils")
+import noise
+from metrics import SNR
+from tools import find_oracle_threshold,denoise_spectral_sub,demucs_denoise,deepinv_sure_param_search
+
+#Creation du dossier de l'experience
+path="../../../results/"+datetime.now().strftime("%Y%m%d_%H%M")+"_experience_oSNR_sigma_sure_soustraction_spectrale_demucs"
+os.mkdir(path)
+
+CSV_export=[]
+
+#Requete des donnees params.txt
+parameters=open('params_multiple.txt')
+params={}
+for full_line in parameters:
+    line=full_line.strip()
+    if not line[0]=='#':
+        key,value=line.split('=',1)
+        val=value.strip().split(',')
+        if len(val)!=1:
+            for i in range(len(val)):
+                val[i]=float(val[i].strip())
+        else:
+            val=val[0].strip()
+            if val.isdigit():
+                val=int(val)
+            elif val=='True':
+                val=True
+            elif val=='False':
+                val=False
+            else:
+                try:
+                    val=float(val)
+                except:
+                    val=val
+        params[key.strip()]=val
+
+if type(params['isnr'])!=list:
+    params['isnr']=[params['isnr']]
+
+params['sigma']=np.logspace(-3,0,30)
+params['drywet']=np.linspace(0,1,10)
+
+#Import du dossier sonore
+sound_folder='../../../data/'+params['pure_sound_folder']
+sound_files=os.listdir(sound_folder)
+
+sound_files_clean=[]
+for file in sound_files:
+    if file.endswith('.wav'):
+        sound_files_clean.append(file)
+
+#Initialisation des resultats
+all_oSNR={}
+all_oSNR_demucs={}
+all_sure_thresholds={}
+all_sure_drywet={}
+all_sigma_true={}
+
+#Parcours la liste des iSNR de params.txt
+for isnr in params['isnr']:
+
+    all_oSNR[isnr]=[]
+    all_oSNR_demucs[isnr]=[]
+    all_sure_thresholds[isnr]=[]
+    all_sure_drywet[isnr]=[]
+    all_sigma_true[isnr]=[]
+
+    #Parcours les fichiers sonores
+    for sound_file in sound_files_clean:
+
+        #Import du fichier sonore
+        info=sf.info(sound_folder+'/'+sound_file)
+        samplerate=info.samplerate
+
+        duration_extract=10
+        extract_length=duration_extract*samplerate
+
+        if info.frames>extract_length:
+            start=np.random.randint(0,info.frames-extract_length)
+            sound,samplerate=sf.read(sound_folder+'/'+sound_file,start=start,frames=extract_length)
+        else:
+            sound,samplerate=sf.read(sound_folder+'/'+sound_file)
+        if len(sound.shape)>1:
+            sound=np.mean(sound,axis=1)
+
+        sound=convert_audio(torch.from_numpy(sound).float()[None,:],samplerate,16000,1)[0].detach().cpu().numpy()
+        samplerate=16000
+
+        #Bruitage
+        noisy_sound,sigma_true=noise.add_white_noise(sound,samplerate,isnr)
+
+        #Creation de la liste des seuils a tester dans SURE
+        oracle_threshold,figure,thresholds,oSNR_threshold_list=find_oracle_threshold(
+            sound,
+            noisy_sound,
+            sigma_true,
+            samplerate,
+            denoise_spectral_sub,
+            True
+        )
+        plt.close(figure)
+
+        oSNR_sigma_list=[]
+        oSNR_sigma_list_demucs=[]
+        sure_threshold_list=[]
+        sure_drywet_list=[]
+
+        #Parcours des sigma donnes a SURE
+        for sigma in params['sigma']:
+
+            #Recherche du seuil SURE avec ce sigma
+            denoised_sure,sure_threshold,sure_values=deepinv_sure_param_search(
+                y_np=noisy_sound,
+                param_values=thresholds,
+                sigma=sigma,
+                fs=samplerate,
+                denoise_func=denoise_spectral_sub,
+                chunk_size=16384,
+                tau=0.01,
+                n_sure_repeats=1,
+                param_name='threshold'
+            )
+
+            #Calcul oSNR
+            osnr=SNR(sound,denoised_sure)
+
+            oSNR_sigma_list.append(osnr)
+            sure_threshold_list.append(sure_threshold)
+
+            #Recherche du dry/wet SURE avec ce sigma pour DEMUCS
+            denoised_sure_demucs,sure_drywet,sure_values_demucs=deepinv_sure_param_search(
+                y_np=noisy_sound,
+                param_values=params['drywet'],
+                sigma=sigma,
+                fs=samplerate,
+                denoise_func=demucs_denoise,
+                chunk_size=16384,
+                tau=0.01,
+                n_sure_repeats=1,
+                param_name='drywet'
+            )
+
+            #Calcul oSNR DEMUCS
+            osnr_demucs=SNR(sound,denoised_sure_demucs)
+
+            oSNR_sigma_list_demucs.append(osnr_demucs)
+            sure_drywet_list.append(sure_drywet)
+
+        #Graphe individuel oSNR en fonction de sigma
+        sigma_array=np.array(params['sigma'])
+        oSNR_sigma_array=np.array(oSNR_sigma_list)
+        oSNR_sigma_array_demucs=np.array(oSNR_sigma_list_demucs)
+
+        mask=sigma_array>0
+
+        figure=plt.figure(figsize=(8,5))
+
+        plt.semilogx(
+            sigma_array[mask],
+            oSNR_sigma_array[mask],
+            label='Soustraction spectrale, iSNR = '+str(isnr)+' dB'
+        )
+
+        plt.semilogx(
+            sigma_array[mask],
+            oSNR_sigma_array_demucs[mask],
+            label='DEMUCS, iSNR = '+str(isnr)+' dB'
+        )
+
+        plt.axvline(
+            sigma_true,
+            color='black',
+            linestyle='--',
+            linewidth=2,
+            label='True sigma'
+        )
+
+        plt.xlabel('Sigma given to SURE')
+        plt.ylabel('oSNR')
+        plt.title('oSNR as function of sigma given to SURE - '+sound_file)
+        plt.grid(True,which='both')
+        plt.legend()
+
+        safe_sound_file=os.path.splitext(sound_file)[0].replace(' ','_')
+
+        figure.savefig(
+            path+"/individual_oSNR_sigma_"+safe_sound_file+"_iSNR_"+str(isnr)+".jpg",
+            dpi=300,
+            bbox_inches="tight"
+        )
+
+        plt.close(figure)
+
+        all_oSNR[isnr].append(oSNR_sigma_list)
+        all_oSNR_demucs[isnr].append(oSNR_sigma_list_demucs)
+        all_sure_thresholds[isnr].append(sure_threshold_list)
+        all_sure_drywet[isnr].append(sure_drywet_list)
+        all_sigma_true[isnr].append(sigma_true)
+
+#Nombre d'extraits
+N=len(sound_files_clean)
+
+#Creation de la figure finale
+plt.figure(figsize=(8,5))
+
+#Export du CSV
+CSV_export.append([
+    'iSNR',
+    'Sigma given to SURE',
+    'Mean oSNR SpecSub',
+    'Standard deviation oSNR SpecSub',
+    'Uncertainty oSNR SpecSub',
+    'Mean SURE threshold',
+    'Std SURE threshold',
+    'Uncertainty SURE threshold',
+    'Mean oSNR DEMUCS',
+    'Standard deviation oSNR DEMUCS',
+    'Uncertainty oSNR DEMUCS',
+    'Mean SURE drywet',
+    'Std SURE drywet',
+    'Uncertainty SURE drywet',
+    'Mean true sigma',
+    'Std true sigma',
+    'Uncertainty true sigma',
+    'Number of extracts'
+])
+
+for isnr in params['isnr']:
+
+    oSNR_array=np.array(all_oSNR[isnr])
+    oSNR_array_demucs=np.array(all_oSNR_demucs[isnr])
+    sure_thresholds_array=np.array(all_sure_thresholds[isnr])
+    sure_drywet_array=np.array(all_sure_drywet[isnr])
+    sigma_true_array=np.array(all_sigma_true[isnr])
+    sigma_array=np.array(params['sigma'])
+
+    mean_oSNR=np.mean(oSNR_array,axis=0)
+
+    if N>1:
+        std_oSNR=np.std(oSNR_array,axis=0,ddof=1)
+    else:
+        std_oSNR=np.zeros(len(mean_oSNR))
+
+    uncertainty=std_oSNR/np.sqrt(N)
+
+    mean_oSNR_demucs=np.mean(oSNR_array_demucs,axis=0)
+
+    if N>1:
+        std_oSNR_demucs=np.std(oSNR_array_demucs,axis=0,ddof=1)
+    else:
+        std_oSNR_demucs=np.zeros(len(mean_oSNR_demucs))
+
+    uncertainty_demucs=std_oSNR_demucs/np.sqrt(N)
+
+    mean_sure_threshold=np.mean(sure_thresholds_array,axis=0)
+
+    if N>1:
+        std_sure_threshold=np.std(sure_thresholds_array,axis=0,ddof=1)
+    else:
+        std_sure_threshold=np.zeros(len(mean_sure_threshold))
+
+    sure_uncertainty=std_sure_threshold/np.sqrt(N)
+
+    mean_sure_drywet=np.mean(sure_drywet_array,axis=0)
+
+    if N>1:
+        std_sure_drywet=np.std(sure_drywet_array,axis=0,ddof=1)
+    else:
+        std_sure_drywet=np.zeros(len(mean_sure_drywet))
+
+    sure_drywet_uncertainty=std_sure_drywet/np.sqrt(N)
+
+    mean_sigma_true=np.mean(sigma_true_array)
+
+    if N>1:
+        std_sigma_true=np.std(sigma_true_array,ddof=1)
+    else:
+        std_sigma_true=0
+
+    sigma_true_uncertainty=std_sigma_true/np.sqrt(N)
+
+    mask=sigma_array>0
+
+    plt.semilogx(
+        sigma_array[mask],
+        mean_oSNR[mask],
+        label='Soustraction spectrale, iSNR = '+str(isnr)+' dB'
+    )
+
+    plt.fill_between(
+        sigma_array[mask],
+        mean_oSNR[mask]-uncertainty[mask],
+        mean_oSNR[mask]+uncertainty[mask],
+        alpha=0.2
+    )
+
+    plt.semilogx(
+        sigma_array[mask],
+        mean_oSNR_demucs[mask],
+        label='DEMUCS, iSNR = '+str(isnr)+' dB'
+    )
+
+    plt.fill_between(
+        sigma_array[mask],
+        mean_oSNR_demucs[mask]-uncertainty_demucs[mask],
+        mean_oSNR_demucs[mask]+uncertainty_demucs[mask],
+        alpha=0.2
+    )
+
+    plt.axvline(
+        mean_sigma_true,
+        color='black',
+        linestyle='--',
+        linewidth=2,
+        label='Mean true sigma, iSNR = '+str(isnr)+' dB'
+    )
+
+    plt.axvspan(
+        max(mean_sigma_true-sigma_true_uncertainty,np.min(sigma_array[mask])),
+        mean_sigma_true+sigma_true_uncertainty,
+        color='black',
+        alpha=0.15
+    )
+
+    for i in range(len(sigma_array)):
+        CSV_export.append([
+            isnr,
+            sigma_array[i],
+            mean_oSNR[i],
+            std_oSNR[i],
+            uncertainty[i],
+            mean_sure_threshold[i],
+            std_sure_threshold[i],
+            sure_uncertainty[i],
+            mean_oSNR_demucs[i],
+            std_oSNR_demucs[i],
+            uncertainty_demucs[i],
+            mean_sure_drywet[i],
+            std_sure_drywet[i],
+            sure_drywet_uncertainty[i],
+            mean_sigma_true,
+            std_sigma_true,
+            sigma_true_uncertainty,
+            N
+        ])
+
+plt.xlabel('Sigma given to SURE')
+plt.ylabel('Mean oSNR')
+plt.title('Mean oSNR as function of sigma given to SURE - Soustraction Spectrale + DEMUCS')
+plt.grid(True,which='both')
+plt.legend()
+plt.savefig(path+"/mean_oSNR_as_function_of_sigma_sure.jpg",dpi=300,bbox_inches="tight")
+plt.show()
+
+#Export du CSV du graphe
+with open(path+'/Mean oSNR as function of sigma SURE.csv','w') as csvfile:
+    writer=csv.writer(csvfile)
+    writer.writerows(CSV_export)
--- code/utils/tools.py
+++ code/utils/tools.py
@@ -3,6 +3,19 @@ import numpy as np
 from scipy.signal import stft, istft
 from IPython.display import Audio, display
 from metrics import SNR
+import torch
+from denoiser import pretrained
+from denoiser.dsp import convert_audio
+
+#Recherche GPU/CPU
+device = torch.device("cpu")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+if hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = torch.device("mps")
+
+
+
 
 def signal(x,fs,show=False):
     '''
@@ -98,15 +111,18 @@ def denoise_spectral_sub(y,sigma,threshold,fs):
     --- Out ---
     x : signal apres soustraction spectrale (ndarray)
     '''
+    
+    '''
     maxi=50
-    y_t,y_f,y_Sgram=spectrogram(y,fs)
+    
     Sy=np.abs(y_Sgram)**2
     Sb=white_noise_DSP(sigma=sigma,signal_length=len(y),fs=fs)
     Sx=np.maximum(Sy-threshold*maxi*Sb,0)
     H=Sx/Sy
     #mask=np.abs(y_Sgram)>=threshold*sigma**2
     y_Sgram_mask= np.sqrt(H)*y_Sgram
-    
+    '''
+    y_t,y_f,y_Sgram=spectrogram(y,fs)
     lamb=threshold*np.max(y_Sgram)
     y_Sgram_mask=y_Sgram*np.maximum(1-(lamb/np.abs(y_Sgram))**2,np.ones_like(y_Sgram)*0)
     
@@ -164,12 +180,10 @@ def find_oracle_threshold(x,xb,sigma,fs,denoise_func,graph=False):
     return oracle_threshold
 
 #SURE (cf ChatGPT)
-import numpy as np
-import torch
 from torch import nn
 import deepinv as dinv
 
-
+'''
 class SpectralSubNumpyWrapper(nn.Module):
     """
     Wraps your actual NumPy spectral subtraction function so DeepInv can call it.
@@ -336,3 +350,247 @@ def deepinv_sure_spectral_sub_threshold_search(
     best_denoised = denoised_chunks.reshape(-1)[:original_len]
 
     return best_denoised, best_threshold, sure_values
+'''
+
+class NumpyDenoiserWrapper(nn.Module):
+    def __init__(self, denoise_func, sigma, param, fs):
+        super().__init__()
+        self.denoise_func = denoise_func
+        self.sigma = float(sigma)
+        self.param = float(param)
+        self.fs = int(fs)
+
+    def forward(self, y, *args, **kwargs):
+        input_device = y.device
+        input_dtype = y.dtype
+
+        y_cpu = y.detach().cpu().numpy()
+        out = np.zeros_like(y_cpu)
+
+        batch_size = y_cpu.shape[0]
+
+        for i in range(batch_size):
+            signal_1d = y_cpu[i, 0, 0, :]
+
+            _, denoised_1d = self.denoise_func(
+                signal_1d,
+                self.sigma,
+                self.param,
+                self.fs
+            )
+
+            denoised_1d = np.asarray(denoised_1d, dtype=y_cpu.dtype)
+
+            if len(denoised_1d) > len(signal_1d):
+                denoised_1d = denoised_1d[:len(signal_1d)]
+            elif len(denoised_1d) < len(signal_1d):
+                denoised_1d = np.pad(
+                    denoised_1d,
+                    (0, len(signal_1d) - len(denoised_1d)),
+                    mode="constant"
+                )
+
+            out[i, 0, 0, :] = denoised_1d
+
+        return torch.from_numpy(out).to(device=input_device, dtype=input_dtype)
+def deepinv_sure_param_search(
+    y_np,
+    param_values,
+    sigma,
+    fs,
+    denoise_func,
+    chunk_size=16384,
+    device=None,
+    tau=0.01,
+    n_sure_repeats=1,
+    param_name="param",
+):
+    """
+    Finds the SURE-minimizing value of the third denoiser parameter.
+
+    The denoise function must have the format:
+        denoise_func(y, sigma, param, fs) -> (t_denoise, y_denoise)
+
+    Examples
+    --------
+    Spectral subtraction:
+        param = threshold
+
+    Demucs:
+        param = drywet
+
+    SGMSE:
+        param = N, if your SGMSE function interprets the third argument as N
+    """
+
+    if device is None:
+        device = dinv.utils.get_device()
+
+    y_np = np.asarray(y_np, dtype=np.float32)
+
+    if y_np.ndim != 1:
+        raise ValueError("y_np must be a mono 1D NumPy array.")
+
+    if sigma <= 0:
+        raise ValueError("sigma must be positive.")
+
+    param_values = np.asarray(param_values, dtype=float)
+
+    original_len = len(y_np)
+
+    pad_len = (-original_len) % chunk_size
+
+    if pad_len > 0:
+        y_padded = np.pad(y_np, (0, pad_len), mode="reflect")
+    else:
+        y_padded = y_np
+
+    chunks = y_padded.reshape(-1, chunk_size)
+
+    # DeepInv expects image-like tensors: [B, C, H, W]
+    # For 1D audio, we use [chunks, 1, 1, chunk_size]
+    y_tensor = torch.from_numpy(chunks[:, None, None, :]).to(device)
+
+    physics = dinv.physics.Denoising(
+        noise_model=dinv.physics.GaussianNoise(sigma=float(sigma))
+    )
+
+    sure_loss = dinv.loss.SureGaussianLoss(
+        sigma=float(sigma),
+        tau=float(tau)
+    )
+
+    sure_values = []
+
+    for param in param_values:
+
+        model = NumpyDenoiserWrapper(
+            denoise_func=denoise_func,
+            sigma=sigma,
+            param=param,
+            fs=fs
+        ).to(device)
+
+        x_net = model(y_tensor)
+
+        repeated_losses = []
+
+        for _ in range(n_sure_repeats):
+            loss = sure_loss(
+                x_net=x_net,
+                y=y_tensor,
+                physics=physics,
+                model=model,
+            )
+
+            repeated_losses.append(loss.detach().mean().cpu().item())
+
+        sure_scalar = float(np.mean(repeated_losses))
+        sure_values.append(sure_scalar)
+
+    sure_values = np.asarray(sure_values)
+
+    best_idx = int(np.argmin(sure_values))
+    best_param = float(param_values[best_idx])
+
+    print("SURE best index:", best_idx, "/", len(param_values) - 1)
+    print(f"SURE best {param_name}:", best_param)
+    print("SURE first value:", sure_values[0])
+    print("SURE best value:", sure_values[best_idx])
+    print("SURE last value:", sure_values[-1])
+
+    if best_idx == len(param_values) - 1:
+        print(f"WARNING: SURE selected the maximum tested {param_name}.")
+
+    if best_idx == 0:
+        print(f"WARNING: SURE selected the minimum tested {param_name}.")
+
+    best_model = NumpyDenoiserWrapper(
+        denoise_func=denoise_func,
+        sigma=sigma,
+        param=best_param,
+        fs=fs
+    ).to(device)
+
+    with torch.no_grad():
+        x_best = best_model(y_tensor)
+
+    denoised_chunks = x_best.detach().cpu().numpy()[:, 0, 0, :]
+    best_denoised = denoised_chunks.reshape(-1)[:original_len]
+
+    return best_denoised, best_param, sure_values
+
+
+
+#Debruiteurs Deep
+#DEMUCS
+_DEVICE=device
+_MODEL=pretrained.dns64()
+_MODEL.to(_DEVICE)
+_MODEL.eval()
+_MODEL_SR=int(_MODEL.sample_rate)
+
+def demucs_denoise(y,sigma,drywet,fs):
+    _=sigma
+
+    fs=int(fs)
+    drywet=float(drywet)
+    y=np.asarray(y,dtype=np.float32).squeeze()
+
+    y_torch=torch.from_numpy(y).float()[None,:]
+    y_model_sr=convert_audio(y_torch,fs,_MODEL_SR,1)
+
+    y_model_sr=y_model_sr.to(_DEVICE)
+
+    with torch.no_grad():
+        y_hat=_MODEL(y_model_sr[None])
+
+    y_demucs=y_hat[0,0].detach().cpu().numpy()
+    y_noisy=y_model_sr[0].detach().cpu().numpy()
+
+    min_len=min(len(y_demucs),len(y_noisy))
+    y_demucs=y_demucs[:min_len]
+    y_noisy=y_noisy[:min_len]
+
+    y_denoise=drywet*y_demucs+(1.0-drywet)*y_noisy
+    t_denoise=np.arange(len(y_denoise))/_MODEL_SR
+
+    return t_denoise,y_denoise
+
+
+'''
+#DEMUCS
+from denoiser import pretrained #DEMUCS
+from denoiser.dsp import convert_audio
+
+
+def denoise_demucs(y,sigma,threshold,fs)
+    #Detection de GPU/CPU
+    device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Device: {device}")
+    #Import du fichier sonore et modele DEMUCS
+    sound,samplerate=y,fs
+    
+    model = getattr(pretrained, params['demucs_pretrained_model'])()
+    model = model.to(device)
+    model.eval()
+    model_sample_rate=model.sample_rate
+
+    sound_t=torch.from_numpy(sound).float()[None, :]
+    sound=convert_audio(sound_tensor,samplerate,model_sample_rate,1)[0].detach().cpu().numpy()
+
+    
+    sound_t=convert_audio(torch.from_numpy(sound).float()[None, :],samplerate,model_sample_rate,1)[0].detach().cpu().numpy()
+
+    
+    with torch.no_grad():
+
+
+
+        #Debruitage DEMUCS
+        son_bt = torch.from_numpy(son_b).float()[None, :].to(device)
+        son_dt=model(son_bt[None])
+        y_denoise=son_dt[0, 0].detach().cpu().numpy()
+
+    return t_denoise,y_denoise
+    '''
